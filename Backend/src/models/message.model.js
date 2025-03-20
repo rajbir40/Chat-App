53b366f2e511fed29b35c8b26681534962a8227a
Rajbir Singh <rajbir7528@gmail.com>
2025-03-20 14:07:24 +0530

    auth

---
+++ Backend/src/models/message.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const messageSchema  = new mongoose.Schema(
+    {
+    sender : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    receiver : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    text : {
+        type : String,
+    },
+    image : {
+        type : String,
+    },
+},
+{timestamps : true}
+);
+
+module.exports = mongoose.model("Message",messageSchema);
---
+++ Backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+app.use("/api/auth",authRoutes);
+
+app.listen(PORT,()=>{
+    console.log("Running on port:"+PORT);
+    connectDB();
+})
+
